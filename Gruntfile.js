785f78b286701203d80e66509bd188697339ef78
Jeremy Jackson <jeremy@murray>
2017-12-02 17:05:31 -0700

    Overhead of switching to eslint.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,19 @@
-/*jslint node: true, indent: 2 */
-'use strict';
-module.exports = function (grunt) {
-
+module.exports = (grunt) => {
   grunt.initConfig({
-    pkg     : grunt.file.readJSON('package.json'),
-    jslint  : {
-      all     : {
-        src : [ 'package.json', 'Gruntfile.js', 'index.js', 'lib/**/*.js', 'test/**/*.js' ],
-        directives : {
-          indent : 2,
-          node   : true,
-          nomen  : true,
-          regexp : true
-        }
-      }
+    pkg: grunt.file.readJSON('package.json'),
+    eslint: {
+      target: ['package.json', 'Gruntfile.js', 'index.js', 'lib/**/*.js', 'test/**/*.js'],
     },
-    jasmine_nodejs : {
-      options : {
-        specNameSuffix : 'spec.js'
+    jasmine_nodejs: {
+      options: {
+        specNameSuffix: 'spec.js',
       },
-      all     : {
-        specs : [
-          "test/*.spec.js"
-        ]
-      }
-    }
+      all: {
+        specs: [
+          'test/*.spec.js',
+        ],
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-jslint');
@@ -32,9 +21,7 @@ module.exports = function (grunt) {
 
   // Default task(s).
   grunt.registerTask('default', [
-    'jslint',
-    'jasmine_nodejs'
+    'eslint',
+    'jasmine_nodejs',
   ]);
-
 };
-
